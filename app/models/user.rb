a179aba4fb05a2709d55bd2cb79d100b31f01356
GreenWarlock <hector.robles.wolf@gmail.com>
2022-12-01 23:13:38 -0600

    Avances de template y agregar skills a usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
 	has_many :user_skills
+
+	validates :ignition_id, presence: true, uniqueness: true
+	validates :full_name, presence: true
+	validates :department, presence: true
 end
